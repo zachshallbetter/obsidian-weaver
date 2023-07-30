92ee80918e7545d4293b212a3d7875e200dce333
Zach Shallbetter <zachshallbetter@icloud.com>
2023-07-30 13:50:34 -0700

    Migrated manifest creation into a Node script
    It now Reads the package.json file and outputs a manifest.json file in the dist directory. For release, this will have to occur after versioning.

---
+++ scripts/manifest.ts
@@ -0,0 +1,28 @@
+// Run: ts-node scripts/manifest.ts
+// Description: Reads the package.json file and outputs a manifest.json file in the dist directory.
+
+import * as fs from 'fs';
+
+const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
+
+const manifest = {
+    'name' : packageJson.name,
+    'version' : packageJson.version,
+    'description' : packageJson.description,
+    'main' : packageJson.main,
+    'dependencies' : packageJson.dependencies,
+    'devDependencies' : packageJson.devDependencies
+};
+
+const manifestPath = "dist/manifest.json";
+
+if (fs.existsSync(manifestPath)) {
+  const existingManifest = JSON.parse(fs.readFileSync(manifestPath, "utf8"));
+  if (existingManifest.version === manifest.version) {
+    console.log(`Manifest version ${manifest.version} already exists.`);
+    process.exit(0);
+  }
+}
+
+fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
+console.log(`Manifest version ${manifest.version} has been created.`);
